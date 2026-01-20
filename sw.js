07aced8f0f1b44f443586eb09d1b351e7c59975c
Ryan Furtado de Almeida <82188114+RyanFurt12@users.noreply.github.com>
2026-01-20 14:24:01 +0000

    fix: testing pwa

--- sw.js
+++ sw.js
@@ -1,17 +1,16 @@
 const CACHE_NAME = 'habit-v1';
 const ASSETS = [
-  'index.html',
-  'manifest.json'
+    'index.html',
+    'manifest.json'
 ];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+    console.log('Service Worker instalado');
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    );
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
-  );
+    e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
